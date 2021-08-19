8eb050b5b9877c58eda65941c2b21217110a0b0f
Yun HyoJeong <63863627+hyojeongyun@users.noreply.github.com>
2021-08-19 18:08:54 +0900

    비밀 정보

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const app = express()
-const port = 5100
+const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+
+//application/json
+//app.use(bodyParser.json());
+app.use(express.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hyojeong:1234@boilerplate.h5xmx.mongodb.net/boilerplate?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요!!')
+  res.send('Hello World!')
+})
+
+app.post('/register', (req, res)=>{
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
